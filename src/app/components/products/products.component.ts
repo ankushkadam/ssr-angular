c134e8849337bf24b5d05f72cf3ffd6595fe03b5
Ankush Anant Kadam <nkshkdm@gmail.com>
2025-06-03 22:39:37 +0530

    afterNextRender and afterRender hook implemented.

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { afterNextRender, afterRender, Component, inject, OnInit } from '@angular/core';
 import { ListComponent } from '../list/list.component';
 import { GridComponent } from '../grid/grid.component';
 import { ProductService } from '../../services/product.service';
@@ -16,7 +16,48 @@ export class ProductsComponent implements OnInit {
 
   private productService = inject(ProductService)
   
-  productList: Product[] = []
+  productList: Product[] = [];
+
+  constructor(){
+    /**
+     * afterNextRender
+      What it Does:
+      Registers a callback that executes only once, after the next render cycle (after the next change detection and DOM update).
+
+      Use Cases:
+      One-time DOM manipulations, such as initializing third-party libraries.
+
+      Setting up observers (e.g., ResizeObserver, IntersectionObserver) that require the DOM to be present.
+      Performing one-time setup after the initial render
+     */
+
+    afterNextRender(() => {
+      navigator.geolocation.getCurrentPosition(position => {
+      console.log('constructor co-ordinates', position.coords.latitude, position.coords.longitude);
+    });
+
+    console.log('after next render');
+    })
+
+
+    /**
+     * afterRender
+        What it Does:
+        Registers a callback that executes after every render cycle (i.e., after each change detection and DOM update).
+
+        Use Cases:
+        1. Reacting to frequent DOM changes.
+        2. Adjusting element sizes or positions dynamically as content updates.
+        3. Synchronizing state with the DOM after every render.
+
+        Performance Note:
+        Since it runs after every render, use it judiciously to avoid performance issues
+     */
+
+    afterRender(() => {
+      console.log('afterRender');
+    })
+  }
 
   ngOnInit(){
     this.getProductList();
